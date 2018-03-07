b1cbebde0f50c64538c9f405e7ea05b9dd5005a3
jadriennneee <donge_jody@yahoo.com>
2018-03-07 22:03:28 +0800

    Reverted back to previous commit

--- myScript.js
+++ myScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function changeChevronIcon() {
     $('#main-content .always-show').addClass('show').css("height", "auto");
     $('#main-content .always-show').prev().find('.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-up');
@@ -22,27 +21,13 @@ function adjustCollapseView() {
         }
 
         changeChevronIcon();
-=======
-function adjustCollapseView() {
-    var viewportWidth = $(window).width();
-    if (viewportWidth >= 992) {
-        $('.card-header').attr("data-toggle", "");
-        $('#main-content .collapse').addClass('show').css("height", "auto");
-    } else {
-        $('.card-header').attr("data-toggle", "collapse");
-        $('#main-content .collapse').removeClass("show").css("height", 0);
-    }
->>>>>>> 7be46f6c6cbf7f93c49e40de10299429f070a410
 }
 
 $(function() {
     adjustCollapseView();
-<<<<<<< HEAD
     $(window).on("resize", function() {
         if ($(window).width() >= 992) {
             adjustCollapseView();
         }
     });
-=======
->>>>>>> 7be46f6c6cbf7f93c49e40de10299429f070a410
 });
