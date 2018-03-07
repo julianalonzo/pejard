7be46f6c6cbf7f93c49e40de10299429f070a410
julianalonzo <julianaubreya@gmail.com>
2018-03-07 09:10:11 +0800

    Added fonts and tried checkbox (not yet working)

--- myScript.js
+++ myScript.js
@@ -1,15 +1,8 @@
-$('.collapse').on('shown.bs.collapse', function() {
-    $(this).prev().find('.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-up');
-}).on('hidden.bs.collapse', function() {
-    $(this).prev().find('.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
-});
-
 function adjustCollapseView() {
     var viewportWidth = $(window).width();
     if (viewportWidth >= 992) {
         $('.card-header').attr("data-toggle", "");
         $('#main-content .collapse').addClass('show').css("height", "auto");
-        $('.collapse').prev().find('.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
     } else {
         $('.card-header').attr("data-toggle", "collapse");
         $('#main-content .collapse').removeClass("show").css("height", 0);
@@ -18,7 +11,4 @@ function adjustCollapseView() {
 
 $(function() {
     adjustCollapseView();
-    $(window).on("resize", function(){
-        adjustCollapseView();
-    });
 });
